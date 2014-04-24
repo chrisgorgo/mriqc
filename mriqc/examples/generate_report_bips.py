3fbad66dfe4e110b1422af7b8827e6634d5092de
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-04-24 18:12:36 +0200

    cleaning up examples

---
+++ mriqc/examples/generate_report_bips.py
@@ -0,0 +1,81 @@
+from glob import glob
+from mriqc.correlation import get_similarity_distribution
+from mriqc.motion import get_mean_frame_displacement_disttribution
+from mriqc.volumes import get_median_distribution
+from mriqc.reports import create_report
+import pandas as pd
+from nipype.pipeline.engine import Workflow, Node
+from nipype.interfaces.utility import Function
+
+
+if __name__ == '__main__':
+    data_dir = "/scr/adenauer1/internet_study/results/"
+    
+    wf = Workflow("reports")
+    wf.base_dir = "/scr/adenauer1/internet_study/workdir"
+    
+    subjects = [i.split("/")[-1] for i in glob(data_dir + "/*")]
+    subjects.sort()
+     
+    #generating distributions
+    mincost_files = [glob(data_dir + "/%s/preproc/bbreg/*_register.dat.mincost"%(subject))[0] for subject in subjects]
+    similarity_distribution = get_similarity_distribution(mincost_files)
+    
+      
+    realignment_parameters_files = [glob(data_dir + "/%s/preproc/motion/*.par"%(subject))[0] for subject in subjects]
+    mean_FD_distribution, max_FD_distribution = get_mean_frame_displacement_disttribution(realignment_parameters_files)
+      
+    tsnr_files = [glob(data_dir + "/%s/preproc/tsnr/*_tsnr.nii.gz"%(subject))[0] for subject in subjects]
+    mask_files = [data_dir + "/%s/preproc/mask/%s_brainmask.nii"%( subject, subject) for subject in subjects]
+    tsnr_distributions = get_median_distribution(tsnr_files, mask_files)
+     
+    df = pd.DataFrame(zip(subjects, similarity_distribution, mean_FD_distribution, max_FD_distribution, tsnr_distributions), columns = ["subject_id", "coregistration quality", "Mean FD", "Max FD", "Median tSNR"])
+    df.to_csv("/scr/adenauer1/internet_study/summary.csv")
+    
+    similarity_distribution = dict(zip(subjects, similarity_distribution))
+    
+    for subject_id in subjects:
+        print subject_id
+        #setting paths for this subject
+        tsnr_file = glob(data_dir + "/%s/preproc/tsnr/*_tsnr.nii.gz"%(subject_id))[0]
+        
+        realignment_parameters_file = glob(data_dir + "/%s/preproc/motion/*.par"%( subject_id))[0]
+
+        mean_epi_file = glob(data_dir + "/%s/preproc/mean/*.nii.gz"%( subject_id))[0]
+        mask_file = data_dir + "/%s/preproc/mask/%s_brainmask.nii"%( subject_id, subject_id)
+        reg_file = glob(data_dir + "/%s/preproc/bbreg/*_register.dat"%( subject_id))[0]
+        fssubjects_dir = "/scr/adenauer1/internet_study/freesurfer/"
+
+        mincost_file = glob(data_dir + "/%s/preproc/bbreg/*_%s_register.dat.mincost"%(subject_id, subject_id))[0]
+        
+        output_file = "/scr/adenauer1/internet_study/results/%s/report.pdf"%( subject_id)
+        
+        report = Node(Function(input_names=['subject_id', 
+                                             'tsnr_file', 
+                                             'realignment_parameters_file', 
+                                             'mean_epi_file', 
+                                             'mask_file', 
+                                             'reg_file', 
+                                             'fssubjects_dir', 
+                                             'similarity_distribution', 
+                                             'mean_FD_distribution', 
+                                             'tsnr_distributions', 
+                                             'output_file'], 
+                                output_names=['out'],
+                                function = create_report), name="report_%s"%(subject_id).replace(".", "_"))
+        report.inputs.subject_id = subject_id
+        report.inputs.tsnr_file = tsnr_file
+        report.inputs.realignment_parameters_file = realignment_parameters_file
+        report.inputs.mean_epi_file = mean_epi_file
+        report.inputs.mask_file = mask_file
+        report.inputs.reg_file = reg_file
+        report.inputs.fssubjects_dir = fssubjects_dir
+        report.inputs.similarity_distribution = similarity_distribution
+        report.inputs.mean_FD_distribution = mean_FD_distribution
+        report.inputs.tsnr_distributions = tsnr_distributions
+        report.inputs.output_file = output_file
+        report.plugin_args={'override_specs': 'request_memory = 4000'}
+        wf.add_nodes([report])
+              
+    wf.run(plugin="CondorDAGMan")
+         
--- mriqc/examples/generate_report_nki.py
+++ mriqc/examples/generate_report_nki.py
@@ -25,22 +25,24 @@ if __name__ == '__main__':
         #generating distributions
         mincost_files = [data_dir + "results%s/%s/preproc/bbreg/afni_%s_register.dat.mincost"%(tr, subject, subject) for subject in subjects]
         similarity_distribution = get_similarity_distribution(mincost_files)
-        similarity_distribution = dict(zip(subjects, similarity_distribution))
-         
+        
+          
         realignment_parameters_files = [data_dir + "results%s/%s/preproc/motion/afni_realignment_parameters.par"%(tr, subject) for subject in subjects]
-        mean_FD_distribution = get_mean_frame_displacement_disttribution(realignment_parameters_files)
-         
+        mean_FD_distribution, max_FD_distribution = get_mean_frame_displacement_disttribution(realignment_parameters_files)
+          
         tsnr_files = [data_dir + "results%s/%s/preproc/tsnr/%s_r00_afni_tsnr.nii.gz"%(tr, subject, subject) for subject in subjects]
         mask_files = [data_dir + "results%s/%s/preproc/mask/%s_brainmask.nii"%(tr, subject, subject) for subject in subjects]
         tsnr_distributions = get_median_distribution(tsnr_files, mask_files)
+         
+        df = pd.DataFrame(zip(subjects, similarity_distribution, mean_FD_distribution, max_FD_distribution, tsnr_distributions), columns = ["subject_id", "coregistration quality", "Mean FD", "Max FD", "Median tSNR"])
+        df.to_csv("/home/raid3/gorgolewski/Google Drive/nki_quality_reports/%s/summary.csv"%tr)
         
-        df = pd.DataFrame(zip(subjects, similarity_distribution.values(), mean_FD_distribution, tsnr_distributions), columns = ["subject_id", "coregistration quality", "Mean FD", "Median tSNR"])
-        df.to_csv("/scr/adenauer1/PowerFolder/Dropbox/nki_reports/%s/summary.csv"%tr)
+        similarity_distribution = dict(zip(subjects, similarity_distribution))
         
         for subject_id in subjects:
-            print subject_id
             #setting paths for this subject
             tsnr_file = data_dir + "results%s/%s/preproc/tsnr/%s_r00_afni_tsnr.nii.gz"%(tr, subject_id, subject_id)
+            
             timeseries_file = data_dir + "results%s/%s/preproc/output/bandpassed/fwhm_0.0/%s_r00_afni_bandpassed.nii.gz"%(tr, subject_id, subject_id)
             realignment_parameters_file = data_dir + "results%s/%s/preproc/motion/afni_realignment_parameters.par"%(tr, subject_id)
     
@@ -53,31 +55,32 @@ if __name__ == '__main__':
             
             output_file = "/scr/adenauer1/PowerFolder/Dropbox/nki_reports/%s/%s_report.pdf"%(tr, subject_id)
             
-#             report = Node(Function(input_names=['subject_id', 
-#                                                 'tsnr_file', 
-#                                                 'realignment_parameters_file', 
-#                                                 'mean_epi_file', 
-#                                                 'mask_file', 
-#                                                 'reg_file', 
-#                                                 'fssubjects_dir', 
-#                                                 'similarity_distribution', 
-#                                                 'mean_FD_distribution', 
-#                                                 'tsnr_distributions', 
-#                                                 'output_file'], 
-#                                    output_names=['out'],
-#                                    function = create_report), name="report_%s_%s"%(tr,subject_id))
-#             report.inputs.subject_id = subject_id
-#             report.inputs.tsnr_file = tsnr_file
-#             report.inputs.realignment_parameters_file = realignment_parameters_file
-#             report.inputs.mean_epi_file = mean_epi_file
-#             report.inputs.mask_file = mask_file
-#             report.inputs.reg_file = reg_file
-#             report.inputs.fssubjects_dir = fssubjects_dir
-#             report.inputs.similarity_distribution = similarity_distribution
-#             report.inputs.mean_FD_distribution = mean_FD_distribution
-#             report.inputs.tsnr_distributions = tsnr_distributions
-#             report.inputs.output_file = output_file
-#             wf.add_nodes([report])
-#             
-#     wf.run(plugin="CondorDAGMan")
-        
+            report = Node(Function(input_names=['subject_id', 
+                                                 'tsnr_file', 
+                                                 'realignment_parameters_file', 
+                                                 'mean_epi_file', 
+                                                 'mask_file', 
+                                                 'reg_file', 
+                                                 'fssubjects_dir', 
+                                                 'similarity_distribution', 
+                                                 'mean_FD_distribution', 
+                                                 'tsnr_distributions', 
+                                                 'output_file'], 
+                                    output_names=['out'],
+                                    function = create_report), name="report_%s_%s"%(tr,subject_id))
+            report.inputs.subject_id = subject_id
+            report.inputs.tsnr_file = tsnr_file
+            report.inputs.realignment_parameters_file = realignment_parameters_file
+            report.inputs.mean_epi_file = mean_epi_file
+            report.inputs.mask_file = mask_file
+            report.inputs.reg_file = reg_file
+            report.inputs.fssubjects_dir = fssubjects_dir
+            report.inputs.similarity_distribution = similarity_distribution
+            report.inputs.mean_FD_distribution = mean_FD_distribution
+            report.inputs.tsnr_distributions = tsnr_distributions
+            report.inputs.output_file = output_file
+            report.plugin_args={'override_specs': 'request_memory = 4000'}
+            wf.add_nodes([report])
+              
+    wf.run(plugin="CondorDAGMan")
+         
